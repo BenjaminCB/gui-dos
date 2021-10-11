876977d4f3e76deb0fd59840a0fdff23547e3f9c
BenjaminCB <benjamincb@hotmail.dk>
2021-10-11 12:56:56 +0200

    make connection to mysql

---
+++ playground/sql/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string cs = @"server=localhost;userid=user;password=password;database=products; SSL Mode=None";
+
+        using (var con = new MySqlConnection(cs))
+        {
+            con.Open();
+            Console.WriteLine($"MySQL version : {con.ServerVersion}");
+        }
+    }
+}
