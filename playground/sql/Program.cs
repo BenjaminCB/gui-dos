ff10ef6093404db0482185b81f43161469bb7b3d
BenjaminCB <benjamincb@hotmail.dk>
2021-10-11 14:47:47 +0200

    can read and write to database

--- playground/sql/Program.cs
+++ playground/sql/Program.cs
@@ -7,10 +7,33 @@ class Program
     {
         string cs = @"server=localhost;userid=user;password=password;database=products; SSL Mode=None";
 
-        using (var con = new MySqlConnection(cs))
+        var con = new MySqlConnection(cs);
+        con.Open();
+        Console.WriteLine($"MySQL version : {con.ServerVersion}");
+
+
+        // var cmd = new MySqlCommand();
+        // cmd.Connection = con;
+        // cmd.CommandText = "INSERT INTO product VALUE('this', 'is', 'product')";
+        // cmd.ExecuteNonQuery();
+
+
+        // string sql = "INSERT INTO product VALUE(@title, @description, @image)";
+        // var cmd = new MySqlCommand(sql, con);
+        // cmd.Parameters.AddWithValue("@title", "this");
+        // cmd.Parameters.AddWithValue("@description", "is");
+        // cmd.Parameters.AddWithValue("@image", "product");
+        // cmd.ExecuteNonQuery();
+
+        string sql = "SELECT * FROM product";
+        var cmd = new MySqlCommand(sql, con);
+
+        using (MySqlDataReader rdr = cmd.ExecuteReader())
         {
-            con.Open();
-            Console.WriteLine($"MySQL version : {con.ServerVersion}");
+            while (rdr.Read())
+            {
+                Console.WriteLine("{0} {1} {2}", rdr.GetString(0), rdr.GetString(1), rdr.GetString(2));
+            }
         }
     }
 }
