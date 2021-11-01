4e29cb2ffff758cdcf27d17e2b0be6c25347434d
Andreas Tor Mortensen <andr997c@gmail.com>
2021-11-01 16:58:46 +0100

    Refactor Status enums

---
+++ gui-dos/Models/OrderStatus.cs
@@ -0,0 +1,13 @@
+namespace gui_dos.Models
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Accepted,
+        Declined,
+        Assembling,
+        Finished,
+        Delivered
+    }
+
+}
