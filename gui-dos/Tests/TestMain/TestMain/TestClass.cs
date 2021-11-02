ac413f91b7baedde03b81e141f22583b32457cee
Nikolaj Rossander Kristensen <nikolajrk@gmail.com>
2021-11-02 11:25:10 +0100

    Added testing enviroment

---
+++ gui-dos/Tests/TestMain/TestMain/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FsCheck;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TestMain
+{
+    public class TestClass
+    {
+        public Func<int, bool> Test1 = xs => true; 
+
+        
+    }
+
+}
