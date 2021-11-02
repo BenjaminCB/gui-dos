7c227af1d23b21501f8f139788ebee50d2c3068c
BenjaminCB <benjamincb@hotmail.dk>
2021-11-02 13:09:41 +0100

    changes to playground

--- playground/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/ProductsDelete.razor.g.cs
+++ playground/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/ProductsDelete.razor.g.cs
@@ -12,105 +12,105 @@ namespace BlazorApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 1 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 2 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 3 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 4 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 5 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 6 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 7 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 8 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 9 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/tor/Repos/gui-dos/playground/BlazorApp/_Imports.razor"
+#line 10 "/home/bcb/git/gui-dos/playground/BlazorApp/_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/tor/Repos/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
+#line 2 "/home/bcb/git/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
 using System.Collections.Generic;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/tor/Repos/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
+#line 3 "/home/bcb/git/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
 using BlazorApp.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/tor/Repos/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
+#line 4 "/home/bcb/git/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
 using BlazorApp.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/tor/Repos/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
+#line 5 "/home/bcb/git/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
 using BlazorApp.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/tor/Repos/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
+#line 6 "/home/bcb/git/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
 using Microsoft.EntityFrameworkCore;
 
 #line default
@@ -125,7 +125,7 @@ using Microsoft.EntityFrameworkCore;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 40 "/Users/tor/Repos/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
+#line 40 "/home/bcb/git/gui-dos/playground/BlazorApp/Pages/ProductsDelete.razor"
  
     private List<Product> products;
 
