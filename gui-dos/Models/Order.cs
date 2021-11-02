50f88024ebad1cbe552f70276fb30aa3ea449781
KittenSlayerExpress <hejsa2574@gmail.com>
2021-11-02 13:22:17 +0100

    Added mail support, Needs mail format maybe

--- gui-dos/Models/Order.cs
+++ gui-dos/Models/Order.cs
@@ -2,6 +2,9 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System;
+using MailKit.Net.Smtp;
+using MimeKit;
+
 namespace gui_dos.Models
 {
     [Table("orders")]
@@ -19,5 +22,25 @@ namespace gui_dos.Models
         public string PhoneNumber { get; set; }
         public List<GiftBasket> GiftBaskets { get; set; }
         public List<Change> Changelog { get; set; }
+
+        public void SendStatusMail()
+        {
+            var mailMessage = new MimeMessage();
+            mailMessage.From.Add(new MailboxAddress("Isvaerftet", "noreply@Isvaerftet.dk"));
+            mailMessage.To.Add(new MailboxAddress(Name, Email));
+            mailMessage.Subject = "Din odre er nu " + Status;
+            mailMessage.Body = new TextPart("plain")
+            {
+                Text = $"Hej, {Name} din odre er nu {Status}"
+            };
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect ("smtp.gmail.com", 465, true);
+                smtpClient.Authenticate("Username", "Password");
+                smtpClient.Send(mailMessage);
+                smtpClient.Disconnect(true);
+            }
+        }
     }
 }
