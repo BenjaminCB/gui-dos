095920a2fbf84f45c5c3f312361eb1b16cc7261f
BenjaminCB <benjamincb@hotmail.dk>
2021-11-04 13:52:43 +0100

    add price to product

---
+++ gui-dos/Migrations/20211104125046_ProductPriceDouble.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace gui_dos.Migrations
+{
+    public partial class ProductPriceDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "products",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "products",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+    }
+}
