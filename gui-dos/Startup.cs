fbdf713a44d121283ef63d6797101fde7ac51f07
BenjaminCB <benjamincb@hotmail.dk>
2021-11-10 11:26:45 +0100

    login should be working

--- gui-dos/Startup.cs
+++ gui-dos/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using gui_dos.Data;
 using Microsoft.EntityFrameworkCore;
+using gui_dos.Areas.Identity;
 
 namespace gui_dos
 {
@@ -30,6 +31,7 @@ namespace gui_dos
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<Data.ShoppingCart>();
+            services.AddScoped<TokenProvider>();
 
             var cs = $"Data Source={nameof(IsvaerftetDbContext.IsvaerftetDb)}.db";
             services.AddDbContextFactory<IsvaerftetDbContext>(opt =>
@@ -56,9 +58,12 @@ namespace gui_dos
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
