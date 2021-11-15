87a8112add3fbba7960a9e4c9aea813e46f61376
Pederbuus <peterbuussteffensen@gmail.com>
2021-11-15 14:06:22 +0100

    Works ready to merge
    Can now be merged

---
+++ gui-dos/Migrations/20211115113730_UpdateTypes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace gui_dos.Migrations
+{
+    public partial class UpdateTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
