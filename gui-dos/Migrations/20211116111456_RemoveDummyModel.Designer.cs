e5e2a3f595908752200c5dc523fa79140a026027
Andreas Tor Mortensen <andr997c@gmail.com>
2021-11-16 12:17:58 +0100

    Remove dummymodel references and from database

--- gui-dos/Migrations/20211116111456_RemoveDummyModel.Designer.cs
+++ gui-dos/Migrations/20211116111456_RemoveDummyModel.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using gui_dos.Data;
 
 namespace gui_dos.Migrations
 {
     [DbContext(typeof(IsvaerftetDbContext))]
-    partial class IsvaerftetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211116111456_RemoveDummyModel")]
+    partial class RemoveDummyModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
