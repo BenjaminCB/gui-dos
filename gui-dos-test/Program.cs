4799cf795725447cf884b7360781968a0972f2ee
Nikolaj Rossander Kristensen <nikolajrk@gmail.com>
2021-12-06 13:55:27 +0100

    Added tests to database and striporder

--- gui-dos-test/Program.cs
+++ gui-dos-test/Program.cs
@@ -1,46 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
 using FsCheck;
 using FsCheck.Xunit;
 using Xunit;
 using Xunit.Abstractions;
-using gui_dos;
 using Bunit;
+using Microsoft.VisualStudio.CodeCoverage;
+using gui_dos;
+using gui_dos.Forms;
+using gui_dos.Models;
+using gui_dos.Pages;
+using Microsoft.EntityFrameworkCore;
+using gui_dos.Data;
+
 namespace gui_dos.Tests
 {
     
-        public class TestClass
+    public class TestClass
         {
-            public Order shit;
-            public Func<int, bool> FuncToTest = (xs) => true;
-            public Func<int, bool> FuncToFail = (xs) => false;
-            private readonly ITestOutputHelper testOutputHelper;
-            public TestClass(ITestOutputHelper testOutput)
-            {
-                testOutputHelper = testOutput;
-            }
-            [Fact]
-            public void Test1()
-            {
-                Prop.ForAll(FuncToTest).VerboseCheckThrowOnFailure(testOutputHelper);
-            }
-            [Fact]
-            public void Test2()
-            {
-                Prop.ForAll(FuncToFail).VerboseCheckThrowOnFailure(testOutputHelper);
-            }
-            [Fact]
-            public void HelloWorldComponentRendersCorrectly()
-            {
-                // Arrange
-                using var ctx = new TestContext();
-
-                // Act
-                var cut = ctx.RenderComponent<gui_dos.Shared.CartComp>();
-
-                // Assert
-                cut.MarkupMatches("< MudText Align = 'Align.Center' >< strong > Din indkøbskurv er tom.</ strong ></ MudText >");
-            }
+        public List<GiftBasket> genericGiftBaskets;
+        public Order genericOrder;
+        public OrderForm genericOrderForm;
+        public CheckOut checkOutPage;
+        public gui_dos.Data.IsvaerftetDbContext testDb;
+        
+            
+        
+        public Func<int, bool> FuncToTest = (xs) => true;
+        private readonly ITestOutputHelper testOutputHelper;
+        public TestClass(ITestOutputHelper testOutput)
+        {
+            testOutputHelper = testOutput;
         }
+        [Fact]
+        public void Test1()
+        {
+            Prop.ForAll(FuncToTest).VerboseCheckThrowOnFailure(testOutputHelper);
+            
+        }
+        [Fact]
+        public void TestStripOrder()
+        {
+            genericGiftBaskets = new List<Models.GiftBasket>();
+            genericOrder = new Models.Order(genericGiftBaskets);
+            genericOrderForm = new OrderForm();
+            genericOrderForm.FirstName = "Firstname";
+            genericOrderForm.LastName = "Lastname";
+            genericOrderForm.PhoneNumber = "00000000";
+            genericOrderForm.Email = "a@a.a";
+            genericOrderForm.Comment = "comment";
+            genericOrder.FillInformation(genericOrderForm);
+
+            genericOrder.StripInformation();
+            Assert.Equal("",genericOrder.FirstName);
+            Assert.Equal("", genericOrder.LastName);
+            Assert.Equal("", genericOrder.Email);
+            Assert.Equal("", genericOrder.PhoneNumber);
+            Assert.Equal("comment", genericOrder.Comment);
+
+        }
+        [Fact]
+        public void testDatabaseDoesNotChangeOrder()
+        {
+            
+            genericGiftBaskets = new List<Models.GiftBasket>();
+            genericOrder = new Models.Order(genericGiftBaskets);
+            genericOrderForm = new OrderForm
+            {
+                FirstName = "Firstname",
+                LastName = "Lastname",
+                PhoneNumber = "00000000",
+                Email = "a@a.a",
+                Comment = "comment"
+            };
+            genericOrder.FillInformation(genericOrderForm);
+            
+            
+            testDb.Orders.Add(genericOrder);
+            Order datebaseOrder = testDb.Orders.Find("Firstname");
+            Assert.Equal(genericOrder, datebaseOrder);
+
+        }
+    [Fact]
+        public void HelloWorldComponentRendersCorrectly()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<gui_dos.Shared.CartComp>();
+
+            // Assert
+            cut.MarkupMatches("< MudText Align = 'Align.Center' >< strong > Din indkøbskurv er tom.</ strong ></ MudText >");
+        }
+    }
         public class OutputHelper : ITestOutputHelper
         {
             public void WriteLine(string message)
@@ -53,11 +106,7 @@ namespace gui_dos.Tests
                 throw new NotImplementedException();
             }
         }
-        public class Order
-        {
-            public string name;
-            string id;
-        }
+        
         //class Program
         //{
 
